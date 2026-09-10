cb8bdea030060b640cff6c840370db98bd2112c7
AJPerezcossio <joseperezcossio9@gmail.com>
2026-09-10 10:44:03 -0400

    da

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import './App.css'
 function App() {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1> ultimo intento
 
-      <p>Demostración práctica de resolución de conflictos en Git.</p>
-      <footer>hola como estas </footer>
+        <p>Demostración práctica de resolución de conflictos en Git.</p>
+        <footer>hola como estas </footer>
+      </h1>
     </div>
 
   )
