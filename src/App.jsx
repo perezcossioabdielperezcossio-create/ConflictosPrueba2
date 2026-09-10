d0f2b981d09f108368c47e6b099b30c41630f182
Fitooohhh <avm0098@gmail.com>
2026-09-10 09:49:30 -0400

    feat:Commit cambio

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Proyecto Base del Equipo</h1>
+      <h1>Modificacion compañero</h1>
       <p>Demostración práctica de resolución de conflictos en Git.</p>
     </div>
   )
