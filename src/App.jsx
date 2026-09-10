3d415d016c7f7b4a22ce6e887e2ea8be2057c890
Fitooohhh <avm0098@gmail.com>
2026-09-10 10:22:04 -0400

    ultimo intento Fito

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Modificacion compañero</h1>
+      <h1>modificacion ultimo intento</h1>
       <p>Demostración práctica de resolución de conflictos en Git.</p>
     </div>
   )
